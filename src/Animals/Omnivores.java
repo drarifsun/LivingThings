a5c6b07842df326196760aaecf24dbbdf507f535
drarifsun <drarifsun@gmail.com>
2017-12-25 17:13:47 -0500

    Practice of switch case on Omnivores

--- src/Animals/Omnivores.java
+++ src/Animals/Omnivores.java
@@ -12,7 +12,26 @@ public class Omnivores {
 		// TODO Auto-generated method stub
 		System.out.println("Animals living on both vegetables and meat is Omnivores");
 		System.out.println("They have incisors as well as molars");
-		System.out.println("Human is the best example of Omnivores");
+		System.out.println("Human is the best example of Omnivores\n");
+		
+		int name=10;
+		switch(name)
+		{
+		case 1:
+			System.out.println("Human is an omnivore");
+			break;
+			
+		case 2:
+			System.out.println("Baboon is an omnivore");
+			break;
+			
+		case 3:
+			System.out.println("Jackel is not an omnivore");
+			break;
+			
+		default:
+			System.out.println("not in the list of omnivores");
+		}
 	}
 
 }
