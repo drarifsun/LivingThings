cc6384737e09b061ad3a6b55e9196dca5128d67c
drarifsun <drarifsun@gmail.com>
2017-12-25 17:02:47 -0500

    Practising if_else if_else conditional statement in Herbivores

--- src/Animals/Herbivores.java
+++ src/Animals/Herbivores.java
@@ -13,6 +13,23 @@ public class Herbivores {
 		System.out.println("Animals that live on plants and vegetables");
 		System.out.println("They have sharp incisors");
 		System.out.println("Examples: Rabbit, Kangaroo, Cow etc");
+		
+		String name;
+		name="Rabbit";
+		
+		if(name=="Rabbit")
+		{
+			System.out.println("Cute fluffy plant eating animal with sharp incisors");
+		}
+		
+		else if(name=="Kangaroo")
+		{
+			System.out.println("National animal of Australia, carries baby in an abdominal pouch");
+		}
+		
+		else {
+			System.out.println("Animal with canines are not in this category");
+		}
 	}
 
 }
